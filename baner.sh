1c2cdb4807d0117073a9909a0d8c27b904f0908b
jovemsigilosodobembr <114019171+jovemsigilosodobembr@users.noreply.github.com>
2022-10-25 14:06:44 -0300

    Create baner.sh

---
+++ baner.sh
@@ -0,0 +1,9 @@
+cd /data/data/com.termux/files/usr/etc
+rm -rf motd
+cd ~/baneredielsontube
+apt-get install figlet -y
+apt-get install ruby -y
+gem install lolcat 
+cp bash.bashrc /data/data/com.termux/files/usr/etc
+figlet TEMINADO
+termux-open https://www.youtube.com/channel/UCGnyJt1_4MrLbIFDspMCg5w
